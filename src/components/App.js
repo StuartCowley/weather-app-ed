4db390d0641848e4e315796917effe72442948d7
Ed Adkins <ejmadkins@gmail.com>
2022-05-29 20:05:52 +0100

    add LocationDetails

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LocationDetails from "./LocationDetails";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <LocationDetails country= />
     </div>
   );
 }
--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const LocationDetails = ({ city, country }) => {
-  return <div>LocationDetails</div>;
-};
+function LocationDetails(props) {
+  const { city, country } = props;
+  return (
+    <h1 className="location">
+      {city} {country}
+    </h1>
+  );
+}
 
 export default LocationDetails;
