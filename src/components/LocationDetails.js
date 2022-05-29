6481758d6da52afa299e554001a70b8cf79f85af
Ed Adkins <ejmadkins@gmail.com>
2022-05-29 20:54:46 +0100

    add prop-types

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 
 function LocationDetails(props) {
   const { city, country } = props;
-  return (
-    <h1 className="location">
-      {city} {country}
-    </h1>
-  );
+  return <h1 className="location">{`${city} ${country}`}</h1>;
 }
+
+LocationDetails.propTypes = {
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+};
 
 export default LocationDetails;
