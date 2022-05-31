956c8fec953802ea38f1fa9ed63f215a28ebf557
Ed Adkins <ejmadkins@gmail.com>
2022-05-31 18:02:06 +0100

    fix issue with prop-type

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -2,33 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function ForecastDetails({ forecast }) {
-  const formattedDate = new Date(forecast.date).toDateString();
+  const { date, temperature, wind, humidity } = forecast;
+  const formattedDate = new Date(date).toDateString();
   return (
     <div className="forecast-details">
       <div className="forecast-details__date">{formattedDate}</div>
-      <div className="forecast-details__temperature">
-        {forecast.temperature.max}
+      <div className="forecast-details__max-temperature">
+        Max Temperature: {temperature.max}
         &deg;C
       </div>
-      <div className="forecast-details__wind">{forecast.wind.speed}</div>
-      <div className="forecast-details__humidity">{forecast.humidity}</div>
+      <div className="forecast-details__min-temperature">
+        Min Temperature: {temperature.min}
+        &deg;C
+      </div>
+      <div className="forecast-details__humidity">Humidity: {humidity}</div>
+      <div className="forecast-details__wind">
+        Wind: {wind.speed} {wind.direction}
+      </div>
     </div>
   );
 }
 
 ForecastDetails.propTypes = {
-  // eslint-disable-next-line react/no-unused-prop-types
-  date: PropTypes.number.isRequired,
-  temperature: PropTypes.shape({
-    min: PropTypes.number,
-    max: PropTypes.number,
+  forecast: PropTypes.shape({
+    date: PropTypes.number.isRequired,
+    temperature: PropTypes.shape({
+      min: PropTypes.number,
+      max: PropTypes.number,
+    }).isRequired,
+    wind: PropTypes.shape({
+      speed: PropTypes.number,
+      direction: PropTypes.string,
+    }).isRequired,
+    humidity: PropTypes.number.isRequired,
   }).isRequired,
-  wind: PropTypes.shape({
-    speed: PropTypes.number,
-    direction: PropTypes.string,
-  }).isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
-  humidity: PropTypes.number.isRequired,
 };
 
 export default ForecastDetails;
